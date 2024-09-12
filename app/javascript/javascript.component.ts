9513e69f1dc05453a94c84853468b7e38ddb48ba
shashank singh <81452418+singhshashank25@users.noreply.github.com>
2024-09-12 12:35:19 +0530

    Add files via upload

---
+++ app/javascript/javascript.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-javascript',
+  templateUrl: './javascript.component.html',
+  styleUrls: ['./javascript.component.css']
+})
+export class JavascriptComponent{
+  title = "JavaScript";
+
+  constructor(){
+
+  }
+}
